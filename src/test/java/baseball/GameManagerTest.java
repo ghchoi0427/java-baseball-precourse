0b8af6350a8daef94c03d84522f3f04c6b4eaaed
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-25 18:34:49 +0900

    feat: judge 함수 테스트 작성

--- src/test/java/baseball/GameManagerTest.java
+++ src/test/java/baseball/GameManagerTest.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,9 +9,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameManagerTest {
         GameManager gameManager = new GameManager();
 
+        @Test
         public void overlapNumber(){
                 int[] arr = gameManager.generateNumber();
                 assertTrue(arr[0]!=arr[1] && arr[1]!=arr[2] && arr[0]!=arr[2]);
+
+        }
+
+        @Test
+        public void judgement(){
+                int[] target = new int[]{1,2,3};
+                int[] shot = new int[]{3,4,5};
+
+                int[] result = gameManager.judge(target,shot);
+
+                assertThat(result[0]).isEqualTo(1);
+                assertThat(result[1]).isEqualTo(0);
 
         }
 }
