d2528e5e96d1472e8ef4ade8732eeedfe1a676e8
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-25 18:48:07 +0900

    feat: View 클래스, 입력 메서드 생성

---
+++ src/main/java/utils/View.java
@@ -0,0 +1,24 @@
+package utils;
+
+import baseball.GameManager;
+
+import java.util.Scanner;
+
+public class View {
+    GameManager gameManager = new GameManager();
+    Data data = new Data();
+
+    public void inputNumber(Scanner sc) {
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String str = sc.nextLine();
+        int[] shot = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            shot[i] = str.charAt(i);
+        }
+        data.target = gameManager.generateNumber();
+        data.shot = shot;
+
+
+    }
+}
