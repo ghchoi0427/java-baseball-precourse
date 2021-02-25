01a0ee623fff0c6d6430c4defb7bfef452d57900
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-25 13:50:32 +0900

    feat: GameManagerTest.generateNumber 메서드 추가

--- src/test/java/baseball/GameManagerTest.java
+++ src/test/java/baseball/GameManagerTest.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameManagerTest {
+        GameManager gameManager = new GameManager();
 
+        public void generateNumber(){
+
+        }
 }
