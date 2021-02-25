95ba6de543fd25c3d1d1cd9a18dc002f66f0e68b
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 00:16:42 +0900

    fix: 컨벤션에 맞게 코드 수정

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,8 +1,12 @@
 package baseball;
 
+import utils.Data;
 import utils.RandomUtils;
+import utils.View;
 
 public class GameManager {
+    View view = new View();
+    Data data = view.data;
 
     public int[] generateTarget() {
         int[] numbers = new int[3];
@@ -20,28 +24,33 @@ public class GameManager {
     }
 
     public int[] judge(int[] target, int[] shot) {
-        int ball=0;
-        int strike=0;
+        int ball = 0;
+        int strike = 0;
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                ball+=countBall(target[i],i,shot[j],j);
-                strike+=countStrike(target[i],i,shot[j],j);
+                ball += countBall(target[i], i, shot[j], j);
+                strike += countStrike(target[i], i, shot[j], j);
             }
         }
         return new int[]{ball, strike};
     }
 
 
-    public int countBall(int target, int targetIndex, int shot, int shotIndex){
-        if (target == shot && targetIndex != shotIndex)return 1;
+    public int countBall(int target, int targetIndex, int shot, int shotIndex) {
+        if (target == shot && targetIndex != shotIndex) {
+            return 1;
+        }
         return 0;
     }
 
-    public int countStrike(int target, int targetIndex, int shot, int shotIndex){
-        if (target == shot && targetIndex == shotIndex)return 1;
+    public int countStrike(int target, int targetIndex, int shot, int shotIndex) {
+        if (target == shot && targetIndex == shotIndex) {
+            return 1;
+        }
         return 0;
     }
+
 
     public void StopGame() {
 
