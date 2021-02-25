ba3f6c786a3c5815ae8f2451d9cb7974498f82c2
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-26 01:07:51 +0900

    feat: Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,26 @@
 package baseball;
 
+import utils.Data;
+import utils.View;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        View view = new View();
+        GameManager gameManager = new GameManager();
+        Data data = view.data;
         // TODO 구현 진행
+
+        while(data.getCommand()!=2){
+            view.inputNumber(scanner);
+            data.setTarget(gameManager.generateTarget());
+            view.inputNumber(scanner);
+            int[] result = gameManager.judge(data.getTarget(), data.getShot());
+            if(view.printJudge(result)){
+                view.inputCmd(scanner);
+            }
+        }
 
     }
 }
