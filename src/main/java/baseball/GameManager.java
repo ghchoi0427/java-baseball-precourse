2bacd8ba50d2aa9e46e675a3033b21edf1e52076
ghchoi0427 <ghchoi0427@gmail.com>
2021-02-25 13:29:18 +0900

    feat: 크기가 3인 random int 배열을 반환하는 generateNumber 메서드 생성

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -4,5 +4,25 @@ import utils.RandomUtils;
 
 public class GameManager {
 
+    public int[] generateNumber() {
+        int[] numbers = new int[3];
+        numbers[0] = RandomUtils.nextInt(1, 9);
+        numbers[1] = RandomUtils.nextInt(1, 9);
+        numbers[2] = RandomUtils.nextInt(1, 9);
+
+        return numbers;
+    }
+
+    public void startGame() {
+
+    }
+
+    public void judge() {
+
+    }
+
+    public void StopGame() {
+
+    }
 
 }
